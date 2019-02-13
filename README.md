63c066cc2c98c73a616c250d94f91613918058d1
Ankit Agnihotri <ankit@Ankits-MacBook-Pro.local>
2019-02-13 14:14:37 +0530

    hoc demo

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# hoc_demo
+## HOC Demo app
+
+
+Demo application for Higher order components in react
+
+clone the repo
+
+```
+yarn && yarn start
+```
---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Feed from "./components/Feed";
+import "./App.css";
+
+class App extends Component {
+  state = {
+      contacts: []
+  };
+
+  componentDidMount() {
+    fetch("https://api.randomuser.me/?results=50")
+      .then(response => response.json())
+      .then(parsedResponse =>
+        parsedResponse.results.map(user => ({
+          name: `${user.name.first} ${user.name.last}`,
+          email: user.email,
+          thumbnail: user.picture.thumbnail
+        }))
+      )
+      .then(contacts => this.setState({ contacts }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                HOC Demo app
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <Feed contacts={this.state.contacts} />
+      </div>
+    );
+  }
+}
+
+export default App;
